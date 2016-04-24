9f836dff38974e1fc4cf12eea16c17f02b16e107
Boris Morales Rios <anum.bmr.01@gmail.com>
2016-04-24 01:33:33 -0300

    busquedaAvanzada WS

--- src/service/ServiceDAO.java
+++ src/service/ServiceDAO.java
@@ -2,12 +2,10 @@ package service;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
 
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
 
-import org.apache.log4j.Logger;
 import org.orm.PersistentException;
 
 import com.google.gson.Gson;
@@ -42,6 +40,68 @@ public class ServiceDAO {
 		
 		lista = persona.busquedaSimplePersona(cadenaBusqueda);
 		resultado = gson.toJson(lista);
+		
+		return resultado;
+	}
+	
+	@WebMethod(operationName = "busquedaAvanzada")
+	public String busquedaAvanzada(@WebParam(name = "run") String run, 
+			@WebParam(name = "nombre") String nombre, 
+			@WebParam(name = "apellido") String apellido,
+			@WebParam(name = "email") String email,
+			@WebParam(name = "fono") String fono,
+			@WebParam(name = "direccion") String direccion,
+			@WebParam(name = "genero") String genero) throws PersistentException{
+		
+		String resultado = "";
+		List<business.Persona> listaPersonasB = new ArrayList<business.Persona>();
+		business.Persona persona = new business.Persona();
+		
+		//asignaciones
+		if(run != null)
+			persona.setRun(run);
+		else
+			persona.setRun("");
+		
+		if(nombre != null)
+			persona.setNombre(nombre);
+		else
+			persona.setNombre("");
+		
+		if(apellido != null)
+			persona.setApellido(apellido);
+		else
+			persona.setApellido("");
+		
+		if(email != null)
+			persona.setEmail(email);
+		else
+			persona.setEmail("");
+		
+		if(fono != null)
+			persona.setFono(fono);
+		else
+			persona.setFono("");
+		
+		if(direccion != null)
+			persona.setDireccion(direccion);
+		else
+			persona.setDireccion("");
+		
+		if(genero != null)
+			persona.setGenero(genero);
+		else
+			persona.setGenero("");
+		// FIN asignaciones
+		
+		Gson gson = new GsonBuilder().create();
+		
+		try{
+			listaPersonasB = persona.busquedaAvanzada(persona);
+			resultado = gson.toJson(listaPersonasB);
+		}catch(PersistentException p){
+			resultado = null;
+		}
 		
 		return resultado;
 	}
